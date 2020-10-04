de5597665267f6d720e47f617034f3c85fa14f2e
lukaszscislowski <lukas.scislowski@gmail.com>
2020-10-04 16:45:53 +0200

    Add image view

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="searchWrapper">
+      
+      <input
+      id="search"
+      name="search"
+      v-model="searchValue"
+      @input="handleInput" 
+      />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchInput',
+}
+</script>
+
+<style lang="scss" scoped>
+    .searchWrapper{
+        margin-top: 50px;
+        display: flex;
+        flex-direction: column;
+        width: 250px;
+    }
+    label{
+        font-family:'Montserrat', sans-serif
+    }
+    input{
+        height: 30px;
+        border: 0;
+        background: none;
+        border-bottom:1px solid black;
+    }
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import About from './views/About.vue';
 import Search from './views/Search.vue';
 
 Vue.use(Router);
@@ -13,10 +12,6 @@ export default new Router({
             name: 'Search',
             component: Search,
         },
-        {
-            path: '/about',
-            name: 'About',
-            component: About,
-        },
+        
     ],
 }); 
